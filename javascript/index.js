5db96ab8921c2884c2cce883ff7c3bf0a6ce7dea
1qgudu <157238708+1qgudu@users.noreply.github.com>
2024-02-07 13:15:34 +0530

    array exercise added

--- javascript/index.js
+++ javascript/index.js
@@ -33,3 +33,17 @@ console.log(resultAdd);
 console.log(resultSubtract);
 console.log(resultMultiply);
 console.log(resultDivide);
+
+var cricket = ["raju1","rahul3","rahul4","rahul4","rahul5","rahul6","rahul7","rahul8","rahul9","rahul10","rahul11"];
+cricket.shift();
+let x = cricket.length;
+cricket.push("rahul12");
+cricket.sort();
+alert (cricket);
+var cricket1 = ["raju01","rahul03","rahul04","rahul04","rahul05","rahul06","rahul07","rahul08","rahul09","rahul510","rahul011"];
+var jersey = [1,2,3,4,5,6,7,8,9,10,11];
+let u=[];
+for(let i=0;i<=11;i++){
+        u.push([cricket1[i],jersey[i]]);
+    }
+console.log(u);
